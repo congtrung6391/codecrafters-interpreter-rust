d4c894c400e0e4aa01ef9e2176e3c6b20f9a2300
Trung Truong <trung.truong@employmenthero.com>
2024-09-30 23:15:53 +0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,78 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::process::exit;
+
+#[derive(Debug)]
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+}
+
+impl Token {
+    pub fn to_string(&self) {
+        println!("{:#?} {} null", self.token_type, self.lexeme);
+    }
+}
+
+fn add_token(token_type: TokenType, lexeme: String) {
+    let token = Token { token_type, lexeme };
+    token.to_string();
+}
+
+fn lexer_error(line: i32, token: String) {
+    eprintln!("[line {}] Error: Unexpected character: {}", line, token)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,7 +96,8 @@ fn main() {
 
             // Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
-                scanner(file_contents)
+                let result = scanner(file_contents);
+                exit(result);
             } else {
                 println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
             }
@@ -36,22 +109,28 @@ fn main() {
     }
 }
 
-fn scanner(file_contents: String) {
+fn scanner(file_contents: String) -> i32 {
+    let mut result = 0;
+    let line = 1;
     for char in file_contents.chars() {
         match char {
-            '(' => println!("LEFT_PAREN ( null"),
-            ')' => println!("RIGHT_PAREN ) null"),
-            '{' => println!("LEFT_BRACE {{ null"),
-            '}' => println!("RIGHT_BRACE }} null"),
-            ',' => println!("COMMA , null"),
-            ';' => println!("SEMICOLON ; null"),
-            '.' => println!("DOT . null"),
-            '+' => println!("PLUS + null"),
-            '-' => println!("MINUS - null"),
-            '*' => println!("STAR * null"),
-            _ => {}
+            '(' => add_token(TokenType::LEFT_BRACE, String::from(char)),
+            ')' => add_token(TokenType::RIGHT_PAREN, String::from(char)),
+            '{' => add_token(TokenType::LEFT_BRACE, String::from(char)),
+            '}' => add_token(TokenType::RIGHT_BRACE, String::from(char)),
+            ',' => add_token(TokenType::COMMA, String::from(char)),
+            ';' => add_token(TokenType::SEMICOLON, String::from(char)),
+            '+' => add_token(TokenType::PLUS, String::from(char)),
+            '-' => add_token(TokenType::MINUS, String::from(char)),
+            '*' => add_token(TokenType::STAR, String::from(char)),
+            '.' => add_token(TokenType::DOT, String::from(char)),
+            _ => {
+                lexer_error(line, String::from(char));
+                result = 65;
+            }
         }
     }
 
     println!("EOF  null");
+    return result;
 }
